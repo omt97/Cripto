cc90c17a7e937fb2374597cc12e137f0decf6d00
Oriol <mt.oriol97@gmail.com>
2018-11-01 12:59:52 +0100

    mod

--- CosFinit.py
+++ CosFinit.py
@@ -5,25 +5,41 @@ def suma(a,b):
 
     print(result)
 
-def GF_product_p(a,b):
-    aT = -1
-    bT = -1
 
-    result = [0] * (8+8+1)
+def mult(a, b):
+    result = [0] * (8 + 8 + 1)
 
-    print(aT)
-    print(bT)
-
-    for j in range (0, 8):
+    for j in range(0, len(a)):
         if (a[j] == 1):
-            for k in range (0, 8):
+            for k in range(0, len(b)):
                 if (b[k] == 1):
                     if (result[j + k] == 0):
                         result[j + k] = 1
                     else:
                         result[j + k] = 0
 
-    print(result)
+    return result
+
+def modul(a, b):
+    i = 0
+    x = len(a)-1
+    while i == 0:
+        if (a[x] == 1): i = 1
+        else: x = x-1
+
+    if (x < 8): return a
+    else:
+        multi = [0]*(x-8+1)
+        multi[x-8] = 1
+        multi = mult(multi, b)
+        for j in range (0, len(a)):
+            a[j] = (a[j]+multi[j])%2
+        return modul(a, b)
+
+def GF_product_p(a,b):
+    div = [1, 1, 0, 1, 1, 0, 0, 0, 1]
+    return modul(mult(a, b), div)
+
 
 def GF_tables():
     print("a")
@@ -40,4 +56,4 @@ def GF_invers(a):
 if __name__ == '__main__':
     a = [1, 1, 1, 0, 1, 0, 1, 0]
     b = [1, 1, 0, 0, 0, 0, 0, 1]
-    GF_product_p(a, b)
+    print(GF_product_p(a, b))
