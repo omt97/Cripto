65fae655be1c86a1d95cf9244997bbe73d0102c5
Oriol <mt.oriol97@gmail.com>
2018-11-02 03:48:34 +0100

    Ex1

--- CosFinit.py
+++ CosFinit.py
@@ -1,3 +1,6 @@
+import math
+
+
 def suma(a,b):
     result = [0, 0, 0, 0, 0, 0, 0, 0]
     for x in range (0, 8):
@@ -38,22 +41,70 @@ def modul(a, b):
 
 def GF_product_p(a,b):
     div = [1, 1, 0, 1, 1, 0, 0, 0, 1]
-    return modul(mult(a, b), div)
+    return modul(mult(a, b),div)
 
 
 def GF_tables():
-    print("a")
+    TabExp = [0]*256
+    TabLog = [0]*256
+    TabExp[0] = 0x01
+    for i in range (1, 256):
+
+        TabExp[i] = normal(GF_product_p(binario(TabExp[i - 1]), binario(0x03)))
+        TabLog[TabExp[i] & 0xff] = i
+
+    return (TabExp, TabLog)
+
+def binario(a):
+    result = [0]*8
+    for i in range (0,8):
+        b = a%2
+        result[i] = int(b)
+        a = a/2
+
+    return result
+
 
 def GF_product_t(a,b):
     print(a)
 
 def GF_generador():
-    pass
+    tabs = GF_tables()
+    generadores = []
+    generadores = list()
+    contador = 0
+    a = 0
+    power = 0
+    for i in range (0, 256):
+        a = tabs[0][i]
+        power = a
+        contador = 1
+        while power != 0x01:
+            power = normal(GF_product_p(binario(power), binario(a)))
+            contador = contador + 1
+
+        if (contador == 255): generadores.append(a)
+
+    return generadores
 
 def GF_invers(a):
     pass
 
+
+def normal(a):
+    result = 0
+    for i in range(0, 13):
+        if(i == 0): result = result + a[i]
+        else: result = result + (a[i]*2)**i
+    return result
+
+
 if __name__ == '__main__':
+    print(GF_generador())
     a = [1, 1, 1, 0, 1, 0, 1, 0]
     b = [1, 1, 0, 0, 0, 0, 0, 1]
-    print(GF_product_p(a, b))
+    print(normal(GF_product_p(binario(0x97), binario(0x0E))))
+    x = GF_tables()
+    print(x[0])
+    print(x[1])
+    print(x[0][(x[1][0x97]+x[1][0x0E]) % 255])
